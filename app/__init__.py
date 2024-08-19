5bf544d1bc295e0446849b600e5562a29b7eceb0
alaa salahelden <88257685+alaasalahelden20@users.noreply.github.com>
2024-08-19 22:08:19 +0300

    addorginstoback

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,13 @@ from flask_restful import Api
 from models.dp import db, ma
 from flask_restx import Api
 from flask_cors import CORS
+import os
 from controllers.product_controller import ProductNamespace
 from controllers.auth_controller import UserNamespace
 import logging
+from dotenv import load_dotenv
 
+load_dotenv()  # Load environment variables from .en
 
 logging.basicConfig(level=logging.INFO)  # Set level to INFO or DEBUG as needed
 logger = logging.getLogger(__name__)
@@ -19,8 +22,9 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     api = Api(app, version='1.0', title='Product managment System', description='A Product managment application')
+    allowed_origins = os.getenv('ALLOWED_ORIGINS').split(',')
 
-    CORS(app, supports_credentials=True, resources={r"/*": {"origins":  "https://product-mangment-r2kjpkjaf-alaa-salaheldens-projects.vercel.app/"}})
+    CORS(app, supports_credentials=True, resources={r"/*": {"origins": allowed_origins}})
     db.init_app(app)
     jwt = JWTManager(app)
     jwt._set_error_handler_callbacks(api)
